650b9ad35d751cad3ff4cd5792a5f46d610f7aeb
Peter Raasthøj <praa@itu.dk>
2024-11-19 18:06:26 +0100

    Added base structure for project - util package for handling utility such as loading in data or other small features.
    Added test structure for project

---
+++ Test/gameOfLifeTest/utilTest/utilTest.java
@@ -0,0 +1,22 @@
+package gameOfLifeTest.utilTest;
+
+import gameOfLife.util.DataHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class utilTest
+{
+    @BeforeEach
+    public void setup()
+    {
+
+    }
+
+    @Test
+    public void fileReadTest()
+    {
+        DataHandler dh = new DataHandler();
+
+
+    }
+}
---
+++ src/gameOfLife/util/DataHandler.java
@@ -0,0 +1,18 @@
+package gameOfLife.util;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class DataHandler
+{
+    public DataHandler()
+    {
+
+    }
+
+    public void loadFile(File file)
+    {
+
+    }
+
+}
