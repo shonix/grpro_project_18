23ad5054208cc8b3c4722a33a26e56f43415bfb0
Frederik Juul <freju@itu.dk>
2024-11-22 15:51:28 +0100

    - added a class constant for determining when a rabbit is hungry. - added an enum Action, for all possible actions the rabbit can take. - added determineAction method that returns an Action enum for the action to be taken. - added performAction method that takes the world and an Action enum to perform the appropriate action, by calling other methods for the appropriate Action enum given. - added foundPossibleMate method, that looks for a female rabbit within a certain radius. If it finds one, it sets it as its target and returns true. - act method now calls the performAction() method with the world and Action as determined by determineAction methods as parameters.

--- src/gameOfLife/worldObjects/entities/Rabbit.java
+++ src/gameOfLife/worldObjects/entities/Rabbit.java
@@ -2,16 +2,20 @@ package gameOfLife.worldObjects.entities;
 
 import gameOfLife.worldObjects.Burrow;
 import itumulator.executable.DisplayInformation;
+import itumulator.world.Location;
 import itumulator.world.World;
 
 import java.awt.*;
 import java.util.Map;
+import java.util.Set;
 
 public class Rabbit extends Animal {
     //class fields begin
     public static final int AGE_OF_MATURITY = 60; //3 simulation days
     public static final int MAX_AGE = 240; // 12 simulation days
     public static final double DAILY_ENERGY_REDUCTION = 0.1;
+    public static final double HUNGRY_THRESHOLD = 2.0;
+
     //define on the class, all possible images a rabbit can have
     public static final Color RABBIT_COLOR = new Color(218, 205, 184); //color on top-down world view
     private static final DisplayInformation SMALL_RABBIT = new DisplayInformation(RABBIT_COLOR, "rabbit-small");
@@ -23,11 +27,23 @@ public class Rabbit extends Animal {
     private static final DisplayInformation LARGE_RABBIT_FUNGI = new DisplayInformation(RABBIT_COLOR, "rabbit-fungi");
     private static final DisplayInformation LARGE_RABBIT_FUNGI_SLEEPING = new DisplayInformation(RABBIT_COLOR, "rabbit-fungi-sleeping");
     private static final Map<Integer, DisplayInformation> DISPLAY_INFORMATION_MAP = Map.of(
-            0, SMALL_RABBIT_FUNGI_SLEEPING, 1, SMALL_RABBIT_FUNGI,
-            2, SMALL_RABBIT_SLEEPING, 3, SMALL_RABBIT,
-            4, LARGE_RABBIT_FUNGI_SLEEPING, 5, LARGE_RABBIT_FUNGI,
-            6, LARGE_RABBIT_SLEEPING, 7, LARGE_RABBIT
+            0, SMALL_RABBIT_FUNGI_SLEEPING,
+            1, SMALL_RABBIT_FUNGI,
+            2, SMALL_RABBIT_SLEEPING,
+            3, SMALL_RABBIT,
+            4, LARGE_RABBIT_FUNGI_SLEEPING,
+            5, LARGE_RABBIT_FUNGI,
+            6, LARGE_RABBIT_SLEEPING,
+            7, LARGE_RABBIT
     );
+
+    private enum Action{
+        SLEEP,
+        SEEK_SHELTER,
+        SEEK_FOOD,
+        SEEK_MATE,
+        DEFAULT
+    }
     //class fields end
 
     //instance fields begin
@@ -114,15 +130,52 @@ public class Rabbit extends Animal {
      */
     @Override
     public void act(World world){
-        if(world.isNight()){
-            sleep();
-        }else{
-            isAwake = true;
-            //Something
-        }
+        performAction(world, determineAction(world));
         age(world);
         updateDisplayInformation();
     }
+
+    private Action determineAction(World world){
+        if(world.getCurrentTime() >= 12){
+            return Action.SLEEP;
+        } else if (world.getCurrentTime() >= 8) {
+            return Action.SEEK_SHELTER;
+        }
+        if(actualEnergy < HUNGRY_THRESHOLD) return Action.SEEK_FOOD;
+        if(foundPossibleMate(world)) return Action.SEEK_MATE;
+        return Action.DEFAULT;
+    }
+
+    private void performAction(World world, Action action){
+        switch (action){
+            case Action.SLEEP:
+                sleep();
+                break;
+            case Action.SEEK_SHELTER:
+                throw new UnsupportedOperationException("Not supported yet.");
+            case Action.SEEK_FOOD:
+                throw new UnsupportedOperationException("Not supported yet.");
+            case Action.SEEK_MATE:
+                throw new UnsupportedOperationException("Not supported yet.");
+            default:
+                //stand still
+                break;
+        }
+    }
+
+    private boolean foundPossibleMate(World world){
+        Set<Location> tiles = world.getSurroundingTiles(3);
+        for(Location loc : tiles){
+            if (world.getTile(loc) instanceof Rabbit) {
+                if (((Rabbit) world.getTile(loc)).isFemale()) {
+                    target = ((Rabbit) world.getTile(loc));
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
     /**
      * Method instructing rabbit to die, deleting it from the world.
