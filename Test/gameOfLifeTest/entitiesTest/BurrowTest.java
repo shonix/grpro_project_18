13c55e7d26edd4879ed8b968bce984b9317df33c
Frederik Juul <freju@itu.dk>
2024-11-24 19:53:02 +0100

    Test cases created awaiting implementation

---
+++ Test/gameOfLifeTest/entitiesTest/BurrowTest.java
@@ -0,0 +1,42 @@
+package gameOfLifeTest.entitiesTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BurrowTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    /**
+     * K1-3a project specification test.
+     */
+    @Test
+    void testBurrowPlacedFromFile(){
+        //TODO
+    }
+
+    /**
+     * K1-3b project specification test.
+     */
+    @Test
+    void testBurrowDugByRabbit(){
+        //TODO
+    }
+    /**
+     * K1-3c project specification test.
+     */
+    @Test
+    void testBurrowStandOn(){
+        //TODO
+    }
+
+}
