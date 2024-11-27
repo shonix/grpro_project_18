b3acdbfac5f0bfa806d435c5017d63ff5f206245
Peter <peterfut123@gmail.com>
2024-11-27 17:47:21 +0100

    Updated burrows to use hole-small.png

--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,12 @@ public class Main {
 
     public static void main(String[] args) {
         int resolution = 800;
-        int programDelay = 100;
-        ProgramInitializer pi = new ProgramInitializer("week-1", "t1-2cde", resolution, programDelay);
+        int programDelay = 10;
+        ProgramInitializer pi = new ProgramInitializer("week-1", "test", resolution, programDelay);
         List<Program> programs = pi.getPrograms();
         for(Program p : programs) {
             p.setDisplayInformation(Grass.class, new DisplayInformation(Color.green, "grass"));
-            p.setDisplayInformation(Burrow.class, new DisplayInformation(Color.DARK_GRAY, "hole"));
+            p.setDisplayInformation(Burrow.class, new DisplayInformation(Color.DARK_GRAY, "hole-small"));
             p.show();
 
 
